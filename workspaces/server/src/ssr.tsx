50edd900371027a1f8bf0cea80b979c1a7070836
s1f10200400 <107386266+s1f10200400@users.noreply.github.com>
2025-03-23 16:04:19 +0900

    k

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -51,6 +51,13 @@ export function registerSsr(app: FastifyInstance): void {
     }
 
     const router = createStaticRouter(handler.dataRoutes, context);
+    renderToString(
+      <StrictMode>
+        <StoreProvider createStore={() => store}>
+          <StaticRouterProvider context={context} hydrate={false} router={router} />
+        </StoreProvider>
+      </StrictMode>,
+    );
 
     const rootDir = path.resolve(__dirname, '../../../');
     const imagePaths = [
